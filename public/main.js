61e40df9089c8a26fdb7ce5b97f0776c71d8ab92
LeonAdeoye <leon.adeoye@gmail.com>
2022-03-25 23:10:49 +0800

    first commit

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const { app, BrowserWindow } = require('electron');
+
+require('@electron/remote/main').initialize();
+
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            enableRemoteModule: true
+        }
+    });
+
+    win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () =>
+{
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
+});
+
+app.on('activate', () =>
+{
+    if (BrowserWindow.getAllWindows().length === 0)
+    {
+        createWindow();
+    }
+});
---
+++ src/components/BoilingVerdict.js
@@ -0,0 +1,11 @@
+function BoilingVerdict(props)
+{
+    if(props.celsius >= 100)
+    {
+        return <p>The water is boiling!</p>
+    }
+
+    return <p> The water is not boiling yet.</p>
+}
+
+export default BoilingVerdict;
